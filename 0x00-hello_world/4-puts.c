dd5036f411c0ed79cc5c3f0a30da439c7b8c2574
nabil-200 <nabilibnlugha@gmail.com>
2023-07-13 06:26:21 -0700

    c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(void)
 {
+	/*a function to print a text*/
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
