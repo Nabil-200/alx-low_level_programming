a4792c6b628e5a3872785905726dbe4b41e231a5
nabil-200 <nabilibnlugha@gmail.com>
2023-07-14 07:50:32 -0700

    a lot of code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+   *main - Entry point
+   *Return: Always 0 (Success)
+   */
+int main(void)
+{
+	int n;
+	int c;
+
+	for (n = 48; n <= 57; n++;)
+	{
+		putchar(n);
+	}
+	for (c = 97; c <= 102; c++;)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
