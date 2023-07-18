794ca1bf94d49792b0707bfd9545c039b7aa5a9e
nabil-200 <nabilibnlugha@gmail.com>
2023-07-18 15:48:12 -0700

    fibonacci number

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,23 +6,22 @@
  */
 int main(void)
 {
-	int i;
-	unsigned long int j, k, next, sum;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-		j = 1;
-		k = 2;
-		sum = 0;
+	for (count = 0; count < 50; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
 
-		for (i = 1; i <= 33; ++i)
-		{
-			if (j < 4000000 && (j % 2) == 0)
-			{
-				sum = sum + j;
-			}
-			next = j + k;
-			j = k;
-			k = next;
-		}
-		printf("%lu\n", sum);
-		return (0);
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
 }
