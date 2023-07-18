45f9905bbe5aa72577a9a49dd48bcdfbfffc7f57
nabil-200 <nabilibnlugha@gmail.com>
2023-07-18 07:39:19 -0700

    numbers 8-10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+   *add - Add two intigers
+   *@a: is an int
+   *@b: is an int
+   *Return: Always 0 (Success)
+   */
+
+int add(int a, int b)
+	{
+		int c;
+
+
+		c = a + b;
+		return (c);
+		_putchar('\n');
+	}
