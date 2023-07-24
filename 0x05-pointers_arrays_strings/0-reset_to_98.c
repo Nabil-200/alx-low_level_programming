0647a25ee5cc9820499faed6ca0bfa9c34467dc8
nabil-200 <nabilibnlugha@gmail.com>
2023-07-24 03:46:44 -0700

    questions 0-4

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Takes a pointer and points it to an integer
+ * @n: input from the user
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - print in reverse
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	s--;
+	for (o = i; o > 0; o--)
+	{
+		_putchar(*s);
+
