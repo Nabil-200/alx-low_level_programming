a051d53b3923924dd08095eef065d25f68273261
nabil-200 <nabilibnlugha@gmail.com>
2023-07-31 04:08:59 -0700

    assignments 0,1,2

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_memset - It places a specified constant byte
+ *in a memory
+ *@s: address of specified memory
+ *@b: input value of byte
+ *@n: number of bytes to filled
+*Return: s pointer
+ **/
+char *_memset(char *s, char b, unsigned int n);
+{
+	int a = 0;
+
+	while (n > 0)
+	{
+		a++;
+		s[a] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_memcpy - copy memory from one destination to another
+ *@dest: destination being transfered to
+ *@src: source destination
+ *@n: number of bytes transferred
+ *Return: dest pointer
+ **/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		i++;
+		dest[i] = src[i];
+		n--;
+	}
+	return (dest);
+}
