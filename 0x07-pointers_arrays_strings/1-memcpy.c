7a0eb1f2eb8d614167b479a59d381b7ba7b45c9c
nabil-200 <nabilibnlugha@gmail.com>
2023-07-31 05:47:10 -0700

    changes

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,11 +8,10 @@
  **/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	while (i < n)
+	while (i = 0; i < n; i++)
 	{
-		i++;
 		dest[i] = src[i];
 		n--;
 	}
