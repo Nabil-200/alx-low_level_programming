df7a7e5f28d821653d3c7fd36a20d96dd48d103f
nabil-200 <nabilibnlugha@gmail.com>
2023-08-02 05:55:52 -0700

    puts recursions

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion - accpets a pointer to string and prints it
+ * @s: a string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		while (*s != '\0')
+		{
+			_putchar(*s);
+			_puts_recursion(s + 1);
+		}
+	}	
+
+	else
+		_putchar('\n');
+}
