52b2397a0677525f2a53c3db5119a3cb4206e7ae
nabil-200 <nabilibnlugha@gmail.com>
2023-08-04 09:36:42 -0700

    print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints all arguments it received from user
+ * @argc: Count of argunents passed
+ * @argv: Array of arguments passed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int itr = 0;
+
+	while(itr < argc)
+	{
+		itr++
+		print("%s\n", argv[i]);
+	}
+
+	return (0);
+}
